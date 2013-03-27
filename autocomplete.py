4e5d511b21a9fa396fdb9276cabbbd180c33c24b
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-03-27 12:09:19 -0400

    Base implementation for tries
    - Node class for generic tries
    - function for generating prefixes

--- autocomplete.py
+++ autocomplete.py
@@ -1,88 +1,53 @@
 import unittest
 import string
+import pdb
 
 TEST_FILE = 'whitmanpoem.txt'
 
 def triefy_file(file_name=TEST_FILE):
 	with open(file_name,'r') as f:
-		trial_trie = {}
+		words = []
 		for line in f:
-			words = line.split()
-			for word in words:
-				build_trie(word,trial_trie)
-	return trial_trie
+			words.extend([word.strip(string.punctuation) for word in line.split()])
+	return words
 
-def dict_depth(dictionary, depth=0):
-	"""Gotten from stackoverflow"""
-	if not isinstance(dictionary,dict) or not dictionary:
-		return depth
-	return max(dict_depth(v,depth+1) for k,v in dictionary.iteritems())
+def prefix(li):
+	return [li[:i] for i in range(1,len(li)+1)]
 
-def build_trie(word,trie):
-	letters = [letter.lower() for letter in word if letter in string.letters]
-	old_dict = trie
-	while letters:
-		current_letter, children = letters.pop(0),letters
-		current_dict = old_dict.setdefault(current_letter,{})
-		old_dict = current_dict
 
-	current_dict['END'] = {}
-	return trie
-def pop_letter(word):
-	return word[0], word[1:] # returns letter and the remainder of word
+class Node(object):
+	def __init__(self, value, children=None, isend=False):
+		self.value = value
+		self.children = children or []
+		self.isEnd =isend
 
-def in_trie(word,trie):
-	built_word = ''
-	pointer = 0
-	start_trie = trie
-	current_word = word
-	while current_word:
-		letter, rest_of_word = pop_letter(current_word)
-		new_trie = start_trie.get(letter) # the dictionary in which we will in the next round (for the first letter in 'rest of word')
-		if not new_trie:
-			print "No entries for '{}'\n Only entries for: '{}' \n {}".format(word,built_word,start_trie)
-			return False
-		start_trie = new_trie
-		current_word = rest_of_word
-		built_word+=letter
-		print "Current word portion found: {}".format(built_word)
+	def insert(self, nodes):
+		if nodes == []:
+			return self
+		node,nodes = nodes[0],nodes[1:]
+		child = self.get(node)
+		if not child:
+			self.children.append(node)
+			child = node
+		child.insert(nodes)
+		return self
 
-	if 'END' in new_trie.keys():
-		print "The word {} was found in the corpus".format(built_word)
-		return True
-	else:
-		print "Only supersets of the word exist in the corpus: \n{}".format(new_trie)
-		return False
+	def get(self,node):
+		for child in self.children:
+			if child == node:
+				return child
 
-class TrieTests(unittest.TestCase):
+		return None
 
-	def test_word_check(self):
-		word = "hello"
-		triefied_word = {'h':{'e':{'l':{'l':{'o':{'END':True}}}}}}
+	def __eq__(self,other_node):
+		return self.value == other_node.value
 
-		self.assertEqual(build_trie(word,{}),triefied_word)
-
-	def test_depth(self):
-		word = 'oxford'
-		trie = build_trie(word,{})
-
-		self.assertEqual(dict_depth(trie),len(word)+1)
-
-	def test_with_space(self):
-		"""True if spaces are treated as characters"""
-		words = 'hold up'
-		trie = build_trie(words,{})
-		triefied_words = {'h':{'o':{'l':{'d':{' ':{'u':{'p':{'END':True}}}}}}}}
-
-		self.assertEqual(trie,triefied_words)
-
-	def test_recover_words(self):
-		pass
+	def __repr__(self):
+		return "Node({})".format(self.value)
 
 if __name__ == '__main__':
-	trie = triefy_file()
-	while True:
-		term = raw_input("Please enter a search term: ")
-		in_trie(term,trie)
-
+	root = Node(None)
+	l1 = [Node('c'),Node('ca'),Node('cap')]
+	l2 = [Node('c'),Node('ca'),Node('cat')]
+	pdb.set_trace()
 
