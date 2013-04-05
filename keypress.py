58f15778b2bb9fd3a7cb23aa6df163bb439d7a5a
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-04-04 22:56:33 -0400

    Fixed CPU overuse

--- keypress.py
+++ keypress.py
@@ -1,10 +1,12 @@
 import termios, fcntl, sys, os,time
 import pdb
 
-def _setup():
-	fd = sys.stdin.fileno()
 
+
+def setup():
+	fd = sys.stdin.fileno()
 	oldterm = termios.tcgetattr(fd)
+
 	newattr = termios.tcgetattr(fd)
 	newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
 	termios.tcsetattr(fd,termios.TCSANOW,newattr)
@@ -12,12 +14,14 @@ def _setup():
 	oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
 	fcntl.fcntl(fd,fcntl.F_SETFL,oldflags | os.O_NONBLOCK)
 
-def clean_up():
+	return fd,oldterm,oldflags
+
+def clean_up(fd,oldterm,oldflags):
 	termios.tcsetattr(fd,termios.TCSAFLUSH,oldterm)
 	fcntl.fcntl(fd,fcntl.F_SETFL,oldflags)
 
 def use_letter(fn,**kwargs):
-	_setup()
+	# _setup()
 	print "{} is now on".format(fn.__name__)
 	time.sleep(1)
 	c = ''
@@ -25,7 +29,7 @@ def use_letter(fn,**kwargs):
 		try:
 			key = sys.stdin.read(1)
 		except IOError:
-			pass
+			time.sleep(0.1)
 		else:
 			if key == '\n':
 				c = ''
@@ -33,4 +37,4 @@ def use_letter(fn,**kwargs):
 				c+= key
 				print '\033[35m{}\033[0m---'.format(c)
 				fn(c,**kwargs)
-	clean_up()
+	clean_up(file_no,oldterm)
