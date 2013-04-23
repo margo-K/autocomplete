4a5f4217503fb1ad9084ca9912f9075c9c3d5604
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-04-23 18:30:46 -0400

    Adds prettyprinting for autocomplete results
    * autocomplete function now only returns a list of words starting with the prefix

--- autocomplete.py
+++ autocomplete.py
@@ -2,25 +2,29 @@ import sys
 import time
 from prefixcorpus import Corpus,nodify
 
+
 def autocomplete(prefix,corpus=None,pretty=True):
-	colorprefix = '\t\033[35m{prefix}\033[0m'
 	node = corpus.find(nodify(prefix))
 	if node:
-		found_words =  [str(leaf) for leaf in node.endnodes()]
-		if pretty:
-			for word in found_words:
-				print word.replace(prefix, colorprefix.format(prefix=prefix),1)
+		found_words =  [str(end) for end in node.endnodes()]
 		return found_words
 	else:
 		return None
+
+def autocompleteprint(prefix,word_list):
+	colorprefix = '\t\033[35m{prefix}\033[0m'
+	for word in word_list:
+		print word.replace(prefix, colorprefix.format(prefix=prefix),1)
+
 
 
 if __name__ == '__main__':
 	try:
 		alt_corpus = Corpus('sampletexts/allshakespeare.txt')
 		while True:
-			letters = raw_input("Please enter some letters")
-			autocomplete(prefix=letters,corpus=alt_corpus)
+			letters = raw_input("Please enter some letters: ")
+			words = autocomplete(prefix=letters,corpus=alt_corpus)
+			autocompleteprint(prefix=letters,word_list=words)
 			time.sleep(.5)
 	except KeyboardInterrupt:
 		print "Unsetting autocomplete"
